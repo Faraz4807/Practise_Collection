418fa002c0baa7fbdcd74568194ca892846e7f6d
Faraz4807 <farajm624@mail.com>
2023-09-12 21:28:36 +0530

    Program update

--- TyoeCasting.java
+++ TyoeCasting.java
@@ -1,16 +1,22 @@
 public class TyoeCasting {
     public static void main(String[] args) {
-        
+       
         // Assigning an integer value to a double variable
         int num1 = 45;
         double num2;
         num2 = num1; // Implicit type casting (int to double)
         System.out.println(num2); // Printing the double value of num2, which will be 45.0
-        
+
         // Explicit type casting from double to int
         double t1 = 65.3654;
         int t2;
-        t2 = (int)t1; // Casting t1 (double) to t2 (int)
+        t2 = (int) t1; // Casting t1 (double) to t2 (int)
         System.out.println(t2); // Printing the integer value of t2, which will be 45
+
+        float a1=7;
+        float a2=2;
+        float res=a1/a2;
+        System.out.println(res);
+
     }
 }
